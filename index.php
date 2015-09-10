e8c0d356725b25069a11ceacd770a7fb36ad518a
Ben <wowmaniac2005@hotmail.com>
2015-09-10 22:28:41 +1000

    Updated Database Functionality

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(isset($_REQUEST['action'])) {
 				$MD = ($days/365)*686;
 				$MartianDays = round($MD); 
 				$resultTable .= "<h2>Database Table</h2>
-								<table>
+								<table style='text-align:center;vertical-align:middle'>
 								<tr>
 									<th>ID</th>
 									<th>First Name</th>
@@ -192,7 +192,7 @@ if(isset($_REQUEST['action'])) {
 		<input type="hidden" name="FormSubmit" value="true" />
 		<br/>
 		<div style="text-align:center;vertical-align:middle">
-		<div style="text-align:center;vertical-align:middle"><?=$Firstname;?></div>
+		<?=$Firstname;?>
 		<?=$surname;?>
 		<?=$hasbeenAliveString;?>
 		<?=$MartianDays;?>
