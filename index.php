4893e1d9860090c853b9a8eedc14eb015051c2c5
Ben <wowmaniac2005@hotmail.com>
2015-09-12 15:02:54 +1000

    Swap DB connection to external string

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=d982vlbpdpu737 host=ec2-54-235-162-144.compute-1.amazonaws.com port=5432 user=ydsgglksiqckdh password=SPgVICpqS9AGYXGHwSE65WJLt6 sslmode=require";
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error.";
-    exit;
-} 
+include_once("db.php");
 
 
 $name = '';
